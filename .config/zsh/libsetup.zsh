b3aca914e9c1caa19a71004b4d692b63f471f346
imAliAzhar <ali.azhar@finn.auto>
2023-10-05 22:00:22 +0500

    (brew) update libs list installed

--- .config/zsh/libsetup.zsh
+++ .config/zsh/libsetup.zsh
@@ -1,1 +1,4 @@
-brew install fd fzf ripgrep lf bat
+# brew leaves | xargs echo
+
+brew instll bat eza exercism fd ffmpeg figlet fzf gh jq koekeishiya/formulae/skhd koekeishiya/formulae/yabai lf n neovim nushell ripgrep tmux trash youtube-dl
+
