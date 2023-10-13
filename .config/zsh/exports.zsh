a62c86836415904bfa7aa0f0605751ac08b67b5b
imAliAzhar <ali.azhar@finn.auto>
2023-10-13 20:43:17 +0500

    (zsh) clean up exports and paths

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -1,6 +1,6 @@
 export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/ripgreprc"
 export FZF_DEFAULT_OPTS="--bind ctrl-j:accept"
-HISTTIMEFORMAT="%d/%m/%y %T "
+export HISTTIMEFORMAT="%d/%m/%y %T "
 
 export MYVIMRC=~/.config/nvim/init.vim
 
@@ -11,8 +11,4 @@ export NVM_DIR="$HOME/.nvm"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
 
--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -1,4 +1,11 @@
-export PATH="$HOME/.local/bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="/opt/homebrew/opt/ruby/bin:$PATH"
 export PATH="/opt/homebrew/bin:$PATH"
+
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
+export PATH="$HOME/.local/bin:$PATH"
+
 
