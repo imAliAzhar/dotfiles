17564c13cf5e4fec954fb09755dfcd0556399e5a
imAliAzhar <ali.azhar@finn.auto>
2023-10-27 16:18:43 +0200

    (zellij) add config

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -1,4 +1,4 @@
-if [ -z "$TMUX" ] && [ "$TERM_PROGRAM" != "vscode" ]
+if [ -z "$TMUX" ] && [ "$TERM_PROGRAM" != "vscode" ] && [ "$ZELLIJ" -ne "0" ]
 then
     tmux ls && read tmux_session && tmux attach -t ${tmux_session:-default} || tmux new -s ${tmux_session:-default}
 fi
