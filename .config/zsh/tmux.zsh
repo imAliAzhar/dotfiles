394b718f2bdf1a4c25c4186180dd5c72ff8b0336
imAliAzhar <aliazharthegreat@gmail.com>
2023-11-04 11:00:22 +0100

    (tmux) set git work tree for dotfiles session

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -2,3 +2,12 @@ if [ -z "$TMUX" ] && [ "$TERM_PROGRAM" != "vscode" ] && [ "$ZELLIJ" -ne "0" ]
 then
     tmux ls && read tmux_session && tmux attach -t ${tmux_session:-default} || tmux new -s ${tmux_session:-default}
 fi
+
+# For dotfiles session, copy tmux env vars to shell env
+# tmux env set by ~/.local/bin/tmux-fzf-session
+SESSION=$(tmux display-message -p '#S')
+
+if [[ "$SESSION" == "dotfiles" ]]; then
+    export $(tmux show-environment GIT_DIR)
+    export $(tmux show-environment GIT_WORK_TREE)
+fi
