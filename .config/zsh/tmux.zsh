f20e8d48718ec312a9a7afe3fecefda4fabb71d6
imAliAzhar <aliazharthegreat@gmail.com>
2023-11-06 03:15:21 +0100

    (tmux) disable on terminal.app

--- .config/zsh/tmux.zsh
+++ .config/zsh/tmux.zsh
@@ -1,4 +1,4 @@
-if [ -z "$TMUX" ] && [ "$TERM_PROGRAM" != "vscode" ]
+if [ -z "$TMUX" ] && [ "$TERM_PROGRAM" != "vscode" ] && [ "$TERM_PROGRAM" != "Apple_Terminal" ]
 then
     # tmux ls && read tmux_session && tmux attach -t ${tmux_session:-default} || tmux new -s ${tmux_session:-default}
     source tmux-fzf-session
