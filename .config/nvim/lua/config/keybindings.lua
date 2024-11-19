63dccdf88be9ec6bb1f19802260e046997dda691
imAliAzhar <aliazharthegreat@gmail.com>
2024-11-19 08:02:37 +0100

    (nvim) update lf and lazygit tmux popups to work with nvim

--- .config/nvim/lua/config/keybindings.lua
+++ .config/nvim/lua/config/keybindings.lua
@@ -8,19 +8,18 @@ function copyFileName()
   vim.fn.setreg('+', filename) -- Write to clipboard
 end
 
-
-
-
+function openExplorer()
+  local filepath = vim.fn.expand('%:p')
+  local command = "editor-explorer " .. filepath 
+  vim.fn.system(command)
+end
 
 
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-
+vim.keymap.set({ 'n', 'i', 'v', 'c' }, '<Leader>e', openExplorer, { desc = "Open explorer" })
 vim.keymap.set('n', '<leader>gf', copyFileName, { desc = "Copy current file name", noremap = true, silent = true })
 vim.keymap.set('n', '<leader>gF', copyFilePath, { desc = "Copy current file path", noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
 
 vim.keymap.set({ 'n', 'v' }, "H", "0", { desc = "Go to beginning of line" })
 vim.keymap.set({ 'n', 'v' }, "L", "$", { desc = "Go to end of line" })
