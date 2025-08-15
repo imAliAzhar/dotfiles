c410bc17d551343b34f2fefb4a4675fa6ae914bd
imAliAzhar <aliazharthegreat@gmail.com>
2025-08-15 15:28:45 +0200

    (yazi) light and dark themes, better icon support for links

---
+++ .config/yazi/init.lua
@@ -0,0 +1,11 @@
+---@diagnostic disable: undefined-global
+
+-- Show the link target of the hovered item in the status line
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
--- .config/yazi/ui.lua
+++
@@ -1,7 +0,0 @@
--- https://github.com/sxyazi/yazi/issues/297#issuecomment-1774081158
-
----@diagnostic disable: undefined-global
-
-function Folder:icon(file)
-	return ui.Span(" " .. file:icon() .. "        ")
-end
